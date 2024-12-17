7d2285232e45a9ec72363c1f808cb810e900b822
sebastian-cg <sebastiancarlgomez@gmail.com>
2024-12-17 14:05:42 +0800

    outputs

---
+++ Fragments/app/src/main/java/com/example/fragments/FirstFragment.java
@@ -0,0 +1,32 @@
+package com.example.fragments;
+
+import static android.content.Intent.getIntent;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FirstFragment extends MainActivity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+
+    }
+}
